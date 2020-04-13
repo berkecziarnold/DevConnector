295558af04a44b6442a3185d98c739cb8f33650c
Berkeczi Arnold <arnoldberkeczi@gmail.com>
2020-04-13 14:21:14 +0300

    Logout & Navbar Links

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,6 +5,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from "../actions/types";
 
 const initilaState = {
@@ -37,6 +38,7 @@ export default function (state = initilaState, action) {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
+    case LOGOUT:
       localStorage.removeItem("token");
       return {
         ...state,
